933c54495c170f0282eb89fbe56516e3319077bd
shashivsv <shashibhushanvsv@gmail.com>
2019-01-12 12:18:04 +0530

    update

---
+++ Ashyana.UI.Web/Repository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Entity;
+using Microsoft.Practices.Unity;
+
+namespace Ashyana.UI.Web.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+       // IBaseRepository<T> ModelRepository { get; }
+       // void Save();
+       
+       
+    }
+}
---
+++ Ashyana.UI.Web/Repository/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using Ashyana.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ashyana.UI.Web.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+    
+        private readonly AshyanaDBEntities _context;
+        private IBaseRepository<Model> _modelRepository;
+
+        public UnitOfWork(AshyanaDBEntities context)
+        {
+            _context = context;
+        }
+
+        public IBaseRepository<Model> ModelRepository
+        {
+            get { return _modelRepository ?? (_modelRepository = new BaseRepository<Model>(_context)); }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            System.GC.SuppressFinalize(this);
+        }
+    }
+}
+
+  
